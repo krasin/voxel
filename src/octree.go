8069318691e1fe5daaea73c2c9830d98d700bfb0
Ivan Krasin <imkrasin@gmail.com>
2011-12-05 19:53:11 -0800

    Simplify Octree get/set with the cost of 1/8+1/8^2+1/8^3..

--- src/octree.go
+++ src/octree.go
@@ -1,6 +1,21 @@
 package main
 
-const primeIndexSize = 1 << 24
+import (
+	//	"fmt"
+	"math"
+)
+
+const primeIndexSize = 1 << 25
+
+var octShift = calcOctShift(10)
+
+func calcOctShift(n int) (res []int64) {
+	res = make([]int64, n+1)
+	for i := 1; i <= n; i++ {
+		res[i] = (res[i-1] << 3) + 1
+	}
+	return
+}
 
 type Octree struct {
 	N        int
@@ -12,37 +27,33 @@ type Octree struct {
 
 // N must be power of 2.
 func NewOctree(N int) *Octree {
-	indexSize := N
-	if N > primeIndexSize {
+	volume := int64(N) * int64(N) * int64(N)
+	indexSize := volume
+	if indexSize > primeIndexSize {
 		indexSize = primeIndexSize
 	}
 	return &Octree{
 		N:        N,
-		leafSize: N / indexSize,
+		leafSize: int(volume / indexSize),
 		mask:     int64(indexSize - 1),
 		p:        make([][]uint16, indexSize),
 		v:        make([]uint16, indexSize),
 	}
 }
 
-func (t *Octree) internalGet(depth uint, p [3]int, base [3]int, l int, index int64) uint16 {
-	if t.p[int(index&t.mask)] != nil {
+func (t *Octree) internalGet(depth uint, p, base [3]int, l int, index int64) uint16 {
+	arindex := int((index + octShift[depth]) & t.mask)
+	if t.p[arindex] != nil {
 		panic("extended part of octree is not implemented")
 	}
-	if t.v[int(index&t.mask)] == 0 {
+	if t.v[arindex] == 0 {
 		return 0
 	}
-	if l == 1 {
-		return t.v[int(index&t.mask)] - 1
-	}
-	// We need to check the neighbour if it has the value of 0 or not.
-	// If no, we need to go deeper.
-	// This is because we reuse the cell after an expansion.
-	another := index + (1 << (depth * 3))
-	if another > t.mask || t.v[int(another&t.mask)] == 0 {
+	if l == 1 || t.v[arindex] != 1 {
 		// This is the leaf
-		return t.v[int(index&t.mask)] - 1
+		return t.v[arindex] - 2
 	}
+	// t.v[arindex] == 1 which is the special value means "see subcube"
 	l >>= 1
 	for i := 0; i < 3; i++ {
 		index <<= 1
@@ -56,5 +67,53 @@ func (t *Octree) internalGet(depth uint, p [3]int, base [3]int, l int, index int
 }
 
 func (t *Octree) GetV(x, y, z int) uint16 {
-	return t.internalGet(1, [3]int{x, y, z}, [3]int{0, 0, 0}, t.N, 0)
+	return t.internalGet(0, [3]int{x, y, z}, [3]int{0, 0, 0}, t.N, 0)
+}
+
+func (t *Octree) internalSet(depth uint, p, base [3]int, l int, index int64, v uint16) {
+	//	fmt.Printf("internalSet(depth=%d, p=%v, base=%v, l=%d, index=%d, v=%d)\n", depth, p, base, l, index, v)
+	if v >= math.MaxUint16-1 {
+		panic("v >= math.MaxUint16-1. These values are reserved")
+	}
+	arindex := int((index + octShift[depth]) & t.mask)
+	if t.p[arindex] != nil {
+		panic("extended part of octree is not implemented")
+	}
+	if l == 1 {
+		t.v[arindex] = v + 2
+		return
+	}
+	l >>= 1
+	cur := t.v[arindex]
+	switch {
+	case cur == 0:
+		t.v[arindex] = 1
+	case cur == 1:
+	default:
+		// We need to split the cube into 8 smaller cubes and
+		// recurse into one of them
+		for i := 0; i < 8; i++ {
+			nindex := (index << 3) + int64(i)
+			if nindex+octShift[depth+1] > t.mask {
+				//				fmt.Printf("depth: %d, nindex: %d, depth: %d, octShift[depth+1]: %d, t.mask: %d\n", depth, nindex, depth, octShift[depth+1], t.mask)
+				panic("extended part of octree is not implemented")
+			}
+			arnindex := int((nindex + octShift[depth+1]) & t.mask)
+			//			fmt.Printf("t.v[%d] = %d\n", arnindex, cur)
+			t.v[arnindex] = cur
+		}
+	}
+
+	for i := 0; i < 3; i++ {
+		index <<= 1
+		if p[i] >= base[i]+l {
+			index++
+			base[i] += l
+		}
+	}
+	t.internalSet(depth+1, p, base, l, index, v)
+}
+
+func (t *Octree) Set(x, y, z int, v uint16) {
+	t.internalSet(0, [3]int{x, y, z}, [3]int{0, 0, 0}, t.N, 0, v)
 }
--- src/octree_test.go
+++ src/octree_test.go
@@ -25,6 +25,11 @@ var octreeTests = []octreeTest{
 		512,
 		[]octreeAction{
 			{oGet, [3]int{0, 0, 0}, 0},
+			{oSet, [3]int{0, 0, 0}, 1},
+			{oSet, [3]int{0, 0, 1}, 2},
+			{oSet, [3]int{0, 1, 1}, 3},
+			{oSet, [3]int{1, 1, 1}, 3},
+			{oSet, [3]int{511, 0, 0}, 3},
 		},
 	},
 }
@@ -35,14 +40,15 @@ func TestOctree(t *testing.T) {
 		for actInd, act := range test.Action {
 			failed := false
 			switch act.Op {
+			case oSet:
+				tree.Set(act.P[0], act.P[1], act.P[2], act.V)
+				fallthrough
 			case oGet:
 				v := tree.GetV(act.P[0], act.P[1], act.P[2])
 				if v != act.V {
-					t.Errorf("test #%d, action #d: tree[%v]: want %d, got %d\n", testInd, actInd, act.P, act.V, v)
+					t.Errorf("test #%d, action #%d: tree%v: want %d, got %d\n", testInd, actInd, act.P, act.V, v)
 					failed = true
 				}
-			case oSet:
-				panic("oSet not implemented")
 			}
 			if failed {
 				break
