59171a112f6778ceb5662a9ce2c16933a507bf3b
Ivan Krasin <imkrasin@gmail.com>
2011-12-15 19:18:30 -0800

    Extract stl_reader.go

--- src/optimize.go
+++ src/optimize.go
@@ -5,11 +5,9 @@ import (
 	"io"
 	"image"
 	"image/png"
-	"io/ioutil"
 	"log"
 	"math"
 	"os"
-	"unsafe"
 )
 
 const (
@@ -242,22 +240,6 @@ func WriteNptl(vol BoolVoxelVolume, grid Grid, output io.Writer) (err os.Error) 
 	return
 }
 
-type STLPoint [3]float32
-
-type STLTriangle struct {
-	n STLPoint
-	v [3]STLPoint
-}
-
-func readSTLPoint(a []byte, p *STLPoint) []byte {
-	for i := 0; i < 3; i++ {
-		cur := uint32(a[0]) + uint32(a[1])<<8 + uint32(a[2])<<16 + uint32(a[3])<<24
-		p[i] = *(*float32)(unsafe.Pointer(&cur))
-		a = a[4:]
-	}
-	return a
-}
-
 type Grid struct {
 	P0 [3]float64
 	P1 [3]float64
@@ -267,36 +249,6 @@ type Grid struct {
 type Mesh struct {
 	Grid
 	Triangle []Triangle
-}
-
-func ReadSTL(r io.Reader) (t []STLTriangle, err os.Error) {
-	var data []byte
-	if data, err = ioutil.ReadAll(r); err != nil {
-		return
-	}
-	magic := data[:5]
-	if string(magic) == "solid" {
-		panic("ReadSTL: ascii format is not implemented")
-	}
-	// Skip STL header
-	data = data[80:]
-	n := uint32(data[0]) + uint32(data[1])<<8 + uint32(data[2])<<16 + uint32(data[3])<<24
-	data = data[4:]
-
-	fmt.Fprintf(os.Stderr, "%d triangles\n", n)
-	if len(data) < int(SizeOfSTLTriangle*n) {
-		return nil, fmt.Errorf("ReadSTL: unexpected end of file: want %d bytes to read triangle data, but only %d bytes is available", SizeOfSTLTriangle*n, len(data))
-	}
-	for i := 0; i < int(n); i++ {
-		var cur STLTriangle
-		data = readSTLPoint(data, &cur.n)
-		for j := 0; j < 3; j++ {
-			data = readSTLPoint(data, &cur.v[j])
-		}
-		data = data[2:]
-		t = append(t, cur)
-	}
-	return
 }
 
 func STLToMesh(n int, triangles []STLTriangle) (m Mesh) {
---
+++ src/stl_reader.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"unsafe"
+)
+
+type STLPoint [3]float32
+
+type STLTriangle struct {
+	n STLPoint
+	v [3]STLPoint
+}
+
+func readSTLPoint(a []byte, p *STLPoint) []byte {
+	for i := 0; i < 3; i++ {
+		cur := uint32(a[0]) + uint32(a[1])<<8 + uint32(a[2])<<16 + uint32(a[3])<<24
+		p[i] = *(*float32)(unsafe.Pointer(&cur))
+		a = a[4:]
+	}
+	return a
+}
+
+func ReadSTL(r io.Reader) (t []STLTriangle, err os.Error) {
+	var data []byte
+	if data, err = ioutil.ReadAll(r); err != nil {
+		return
+	}
+	magic := data[:5]
+	if string(magic) == "solid" {
+		panic("ReadSTL: ascii format is not implemented")
+	}
+	// Skip STL header
+	data = data[80:]
+	n := uint32(data[0]) + uint32(data[1])<<8 + uint32(data[2])<<16 + uint32(data[3])<<24
+	data = data[4:]
+
+	fmt.Fprintf(os.Stderr, "%d triangles\n", n)
+	if len(data) < int(SizeOfSTLTriangle*n) {
+		return nil, fmt.Errorf("ReadSTL: unexpected end of file: want %d bytes to read triangle data, but only %d bytes is available", SizeOfSTLTriangle*n, len(data))
+	}
+	for i := 0; i < int(n); i++ {
+		var cur STLTriangle
+		data = readSTLPoint(data, &cur.n)
+		for j := 0; j < 3; j++ {
+			data = readSTLPoint(data, &cur.v[j])
+		}
+		data = data[2:]
+		t = append(t, cur)
+	}
+	return
+}
