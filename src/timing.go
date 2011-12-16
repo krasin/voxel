f1b060f58f86f52cb11531f6ba158269214d61bc
Ivan Krasin <imkrasin@gmail.com>
2011-12-16 15:46:51 -0800

    Add timing routines to measure performance of different steps

---
+++ src/timing.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var starts map[string]time.Time
+var stops map[string]time.Time
+
+func StartTiming(name string) {
+	starts[name] = time.Now()
+}
+
+func StopTiming(name string) {
+	if _, ok := starts[name]; !ok {
+		panic(fmt.Sprintf("StopTiming: unknown timing name: %s", name))
+	}
+	stops[name] = time.Now()
+}
+
+func GetTimings() (res map[string]time.Duration) {
+	for name, start := range starts {
+		if _, ok := stops[name]; !ok {
+			continue
+		}
+		res[name] = stops[name].Sub(start)
+	}
+	return
+}
