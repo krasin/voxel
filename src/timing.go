29c81bcff15c2aa2fe09e660594e3f51e1007f11
Ivan Krasin <imkrasin@gmail.com>
2011-12-16 16:12:25 -0800

    Sort timings by the stop time

--- src/timing.go
+++ src/timing.go
@@ -3,30 +3,46 @@ package main
 import (
 	"fmt"
 	"io"
+	"sort"
 	"time"
 )
 
-var starts map[string]time.Time
-var stops map[string]time.Time
+type Timing struct {
+	Name  string
+	Start time.Time
+	End   time.Time
+}
+
+type TimingSlice []Timing
+
+func (s TimingSlice) Len() int           { return len(s) }
+func (s TimingSlice) Less(i, j int) bool { return s[i].End.Before(s[j].End) }
+func (s TimingSlice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+var timings = make(map[string]Timing)
 
 func StartTiming(name string) {
-	starts[name] = time.Now()
+	timings[name] = Timing{Name: name, Start: time.Now()}
 }
 
 func StopTiming(name string) {
-	if _, ok := starts[name]; !ok {
+	if t, ok := timings[name]; !ok {
 		panic(fmt.Sprintf("StopTiming: unknown timing name: %s", name))
+	} else {
+		t.End = time.Now()
+		timings[name] = t
 	}
-	stops[name] = time.Now()
 }
 
-func GetTimings() (res map[string]time.Duration) {
-	for name, start := range starts {
-		if _, ok := stops[name]; !ok {
-			continue
-		}
-		res[name] = stops[name].Sub(start)
+func (t Timing) Duration() time.Duration {
+	return t.End.Sub(t.Start)
+}
+
+func GetTimings() (res []Timing) {
+	for _, t := range timings {
+		res = append(res, t)
 	}
+	sort.Sort(TimingSlice(res))
 	return
 }
 
