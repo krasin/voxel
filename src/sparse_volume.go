ec20af5c41220ba806dddccbf782e4d2957d7993
Ivan Krasin <imkrasin@gmail.com>
2011-12-19 14:57:43 -0800

    Fix lh=5. Add tests for point2h and h2point.

--- src/sparse_volume.go
+++ src/sparse_volume.go
@@ -2,46 +2,39 @@ package main
 
 type Point16 [3]uint16
 
-type SparseVolume struct {
+const (
 	// Size of leaf cube
-	lh uint
+	lh = 5
 
+	masklh  = (1 << lh) - 1
+	mask3lh = (1 << 3 * lh) - 1
+)
+
+type SparseVolume struct {
 	// Depth of octree
 	lk uint
-
-	// 2 * lh for speed purposes
-	lh2 uint
-
-	// 3 * lh for speed purposes
-	lh3 uint
-
-	// (1 << lh) - 1 for speed purposes
-	masklh uint64
-
-	// (1 << (3*lh))-1 for speed purposes
-	mask3lh uint64
 }
 
 func (v *SparseVolume) point2key(p Point16) uint64 {
-	return uint64(v.point2k(p))<<v.lh3 + uint64(v.point2h(p))
+	return uint64(v.point2k(p))<<(3*lh) + uint64(point2h(p))
 }
 
 func (v *SparseVolume) point2k(p Point16) int {
-	return (spread3(byte(p[0]>>v.lh)) << 2) + (spread3(byte(p[1]>>v.lh)) << 1) + spread3(byte(p[2])>>v.lh)
+	return (spread3(byte(p[0]>>lh)) << 2) + (spread3(byte(p[1]>>lh)) << 1) + spread3(byte(p[2])>>lh)
 }
 
 func (v *SparseVolume) k2point(k int) (p Point16) {
 	panic("k2point not implemented")
 }
 
-func (v *SparseVolume) point2h(p Point16) int {
-	return ((int(p[0]) & int(v.masklh)) << v.lh2) + ((int(p[1]) & int(v.masklh)) << v.lh) + (int(p[2]) & int(v.masklh))
+func point2h(p Point16) int {
+	return ((int(p[0]) & masklh) << (2 * lh)) + ((int(p[1]) & masklh) << lh) + (int(p[2]) & masklh)
 }
 
-func (v *SparseVolume) h2point(h int) (p Point16) {
-	p[0] = uint16(h >> v.lh2)
-	p[1] = uint16((h >> v.lh) & int(v.masklh))
-	p[2] = uint16(h & int(v.masklh))
+func h2point(h int) (p Point16) {
+	p[0] = uint16(h >> (2 * lh))
+	p[1] = uint16((h >> lh) & masklh)
+	p[2] = uint16(h & masklh)
 	return
 }
 
@@ -53,19 +46,19 @@ func spread3(b byte) (x int) {
 	return
 }
 
-func (v *SparseVolume) key2h(key uint64) int {
-	return int(key & v.mask3lh)
+func key2h(key uint64) int {
+	return int(key & mask3lh)
 }
 
 func (v *SparseVolume) key2k(key uint64) int {
-	return int(key >> v.lh3)
+	return int(key >> 3 * lh)
 }
 
 func (v *SparseVolume) key2point(key uint64) (p Point16) {
-	ph := v.h2point(v.key2h(key))
+	ph := h2point(key2h(key))
 	pk := v.k2point(v.key2k(key))
-	p[0] = (pk[0] << v.lh) | ph[0]
-	p[1] = (pk[1] << v.lh) | ph[1]
-	p[2] = (pk[2] << v.lh) | ph[2]
+	p[0] = (pk[0] << lh) | ph[0]
+	p[1] = (pk[1] << lh) | ph[1]
+	p[2] = (pk[2] << lh) | ph[2]
 	return
 }
--- src/sparse_volume_test.go
+++ src/sparse_volume_test.go
@@ -20,9 +20,39 @@ var spread3tests = []spread3test{
 
 func TestSpread3(t *testing.T) {
 	for testInd, test := range spread3tests {
-		has := spread3(test.num)
-		if has != test.want {
-			t.Errorf("test #%d: spread3(%d) returned %d, want %d", testInd, test.num, has, test.want)
+		got := spread3(test.num)
+		if got != test.want {
+			t.Errorf("test #%d: spread3(%d): want %d, got %d", testInd, test.num, test.want, got)
+		}
+	}
+}
+
+type point2htest struct {
+	p               Point16
+	h               int
+	skipReverseTest bool
+}
+
+var point2htests = []point2htest{
+	{Point16{0, 0, 0}, 0, false},
+	{Point16{1, 1, 1}, 0x421, false},
+	{Point16{32, 32, 32}, 0, true},
+	{Point16{31, 31, 31}, (1 << 15) - 1, false},
+	{Point16{15, 31, 4}, 0x3FE4, false},
+}
+
+func TestPoint2h(t *testing.T) {
+	for testInd, test := range point2htests {
+		gotH := point2h(test.p)
+		if gotH != test.h {
+			t.Errorf("test #%d: point2h(%v): want %d, got %d", testInd, test.p, test.h, gotH)
+		}
+		if test.skipReverseTest {
+			continue
+		}
+		gotP := h2point(test.h)
+		if gotP != test.p {
+			t.Errorf("test #%d: h2point(%d): want %v, got %v", testInd, test.h, test.p, gotP)
 		}
 	}
 }
