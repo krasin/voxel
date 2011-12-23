50824c9e3c58c78b4081c2a059b49e4b32f38a5b
Ivan Krasin <imkrasin@gmail.com>
2011-12-22 18:35:28 -0800

    Add comments

--- src/disjoin_set.go
+++ src/disjoin_set.go
@@ -1,14 +1,20 @@
 package main
 
+// DisjoinSet tracks and merges clusters.
+// It might be useful for finding graph components and
+// related problems. See
+// http://en.wikipedia.org/wiki/Union_find for more details.
 type DisjoinSet struct {
 	parent []int
 	rank   []int
 }
 
+// NewDisjoinSet creates an empty DisjoinSet.
 func NewDisjoinSet() *DisjoinSet {
 	return &DisjoinSet{}
 }
 
+// Make makes the new cluster.
 func (s *DisjoinSet) Make() int {
 	x := len(s.parent)
 	s.parent = append(s.parent, x)
@@ -16,6 +22,7 @@ func (s *DisjoinSet) Make() int {
 	return x
 }
 
+// Find returns the root of cluster #x
 func (s *DisjoinSet) Find(x int) int {
 	if s.parent[x] != x {
 		s.parent[x] = s.Find(s.parent[x])
@@ -23,6 +30,7 @@ func (s *DisjoinSet) Find(x int) int {
 	return s.parent[x]
 }
 
+// Join merges two clusters.
 func (s *DisjoinSet) Join(x, y int) {
 	xRoot := s.Find(x)
 	yRoot := s.Find(y)
