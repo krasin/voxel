f540b1f51a66d06a169f93acac16ec2e6ece3dcc
Ivan Krasin <imkrasin@gmail.com>
2012-11-22 10:48:05 +0000

    Remove the last occurence of Uint16Volume (from nptl)

--- nptl/writer.go
+++ nptl/writer.go
@@ -9,26 +9,17 @@ import (
 	"github.com/krasin/voxel/volume"
 )
 
-type Uint16Volume interface {
-	volume.Space
-	Set(x, y, z int, v uint16)
-	GetV(x, y, z int) uint16
-	SetAllFilled(threshold, val uint16)
-	MapBoundary(f func(x, y, z int))
-	Volume() int64
-}
-
-func Write(w io.Writer, vol Uint16Volume, grid raster.Grid) (err error) {
+func Write(w io.Writer, vol volume.Space16, grid raster.Grid) (err error) {
 	stepX := (grid.P1[0] - grid.P0[0]) / float64(vol.N())
 	stepY := (grid.P1[1] - grid.P0[1]) / float64(vol.N())
 	stepZ := (grid.P1[2] - grid.P0[2]) / float64(vol.N())
 
-	vol.MapBoundary(func(x, y, z int) {
-		nv := volume.Normal(vol, g3.Node{x, y, z})
+	vol.MapBoundary(func(node g3.Node) {
+		nv := volume.Normal(vol, node)
 		if _, err = fmt.Fprintf(w, "%f %f %f %f %f %f\n",
-			grid.P0[0]+float64(x)*stepX,
-			grid.P0[1]+float64(y)*stepY,
-			grid.P0[2]+float64(z)*stepZ,
+			grid.P0[0]+float64(node[0])*stepX,
+			grid.P0[1]+float64(node[1])*stepY,
+			grid.P0[2]+float64(node[2])*stepZ,
 			nv[0], nv[1], nv[2]); err != nil {
 			return
 		}
